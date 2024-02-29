6e61a701145577e326001c46d29863494a0e0563
alex lima <delimaferreiraalex@gmail.com>
2024-02-28 21:59:26 -0300

    remove unnecessary lib

--- src/main/java/com/luminahi/publisher_confirms/App.java
+++ src/main/java/com/luminahi/publisher_confirms/App.java
@@ -2,7 +2,6 @@ package com.luminahi.publisher_confirms;
 
 import java.io.IOException;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
